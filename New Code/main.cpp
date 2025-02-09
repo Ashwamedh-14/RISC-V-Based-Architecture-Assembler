2f6424a4b283ad543fb16560734497d604fc53fd
Ashwamedh <113002273+Ashwamedh-14@users.noreply.github.com>
2025-02-09 08:35:10 +0530

    Successfully checked whether the number of registers passed is valid for given opcode

--- New Code/main.cpp
+++ New Code/main.cpp
@@ -5,12 +5,13 @@
 #include <map>
 #include <regex>
 #include <string>
+#include <getopt.h>
 
 using namespace std;
 
 static const vector<char> hex_chars = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 static const map<string,string> op_code = {
-    {"NOP", "0000000"},
+    {"NOP", "00"},
     {"AND", "01"},
     {"OR", "02"},
     {"EXOR", "03"},
@@ -115,7 +116,7 @@ void parse(int line_num, const string &line, ofstream &out_file) {
         temp = strip(temp);
         if (temp.empty()) {
             out_file << "Error: Invalid line at line " << line_num << ".\n";
-            out_file << "Passed Value: \"\"\n";
+            out_file << "Passed Value: \"\".";
             return;
         }
 
@@ -123,31 +124,88 @@ void parse(int line_num, const string &line, ofstream &out_file) {
             if (temp.size() == 2 || temp.size() == 3) {
                 if (instr.reg_num < 3) {
                     instr.registers[instr.reg_num] = temp;
-                    instr.reg_num++;
+                    instr.reg_num++; 
                 } else {
-                    out_file << "Error: Too many registers at line number " << line_num << ".\n";
+                    out_file << "Error: Too many registers at line number " << line_num << "";
                     return;
                 }
             } else {
                 out_file << "Error: Invalid Register at line " << line_num << ".\n";
+                out_file << "Register Passed: " << temp;
                 return;
             }
         } else if (temp.size() == 2) {
             instr.dataline = temp;
         } else {
             out_file << "Error: Invalid Instruction at line number " << line_num << ".\n";
-            out_file << "Passed Value: " << temp << "\n";
+            out_file << "Passed Value: " << temp;
             return;
         }
         line_idx++;
     }
     if (line_idx >= 5) {
         out_file << "Error: Too many instructions at line number: " << line_num << ".\n";
-        out_file << "Number of instructions found: " << line_idx << "\n";
+        out_file << "Number of instructions found: " << line_idx;
         return;
     }
 
-    instr_chk(instr);
+    line_idx = instr_chk(instr);
+
+    switch(line_idx){
+        case 0:
+        cout << "Instruction is correct\n";
+        break;
+
+        case 1:
+        out_file << "Error: Invalid Opcode at line " << line_num << ".\n";
+        out_file << "Opcode: " << instr.opcode;
+        return;
+        break;
+
+        case 2:
+        out_file << "Error: Invalid Dataline at line " << line_num << ".\n";
+        out_file << "Dataline: " << instr.dataline;
+        return;
+        break;
+
+        case 3:
+        case 4:
+        case 5:
+        out_file << "Error: Invalid Register Reference at line " << line_num << ".\n";
+        out_file << "Register Referenced for R" << line_idx - 2 << ": " << instr.registers[line_idx - 3];
+        return;
+        break;
+
+        case 6:
+        case 7:
+        case 8:
+        out_file << "Error: Register R" << line_idx - 6 << " out of Range at line " << line_num << ".\n";
+        out_file << "Register Value: " << instr.registers[line_idx - 6];
+        break;
+    };
+
+    // Verifying whether the number of registers is valid for the given opcode
+    if (instr.reg_num && (op_code.at(instr.opcode) == "00" || op_code.at(instr.opcode) >= "0D" && op_code.at(instr.opcode) <= "11" || op_code.at(instr.opcode) == "1B" || op_code.at(instr.opcode) == "1C")){
+        out_file << "Error: Invalid number of registers for OPCode " << instr.opcode << " at line " << line_num << ".\n";
+        out_file << "Number of Registers Expected: 0, Found: " << instr.reg_num;
+        return;
+    }
+    else if (instr.reg_num != 1 && (op_code.at(instr.opcode) >= "0A" && op_code.at(instr.opcode) <= "0C" || op_code.at(instr.opcode) >= "12" && op_code.at(instr.opcode) <= "15")){
+        out_file << "Error: Invalid number of registers for OPCode " << instr.opcode << " at line " << line_num << ".\n";
+        out_file << "Number of Registers Expected: 1, Found: " << instr.reg_num;
+        return;
+    }
+    else if (instr.reg_num != 2 && (op_code.at(instr.opcode) >= "05" && op_code.at(instr.opcode) <= "09" || op_code.at(instr.opcode) == "16" || op_code.at(instr.opcode) == "17")){
+        out_file << "Error: Invalid number of registers for OPCode " << instr.opcode << " at line " << line_num << ".\n";
+        out_file << "Number of Registers Expected: 2, Found: " << instr.reg_num;
+        return;
+    }
+    else if (instr.reg_num != 3 && (op_code.at(instr.opcode) >= "01" && op_code.at(instr.opcode) <= "04" || op_code.at(instr.opcode) >= "18" && op_code.at(instr.opcode) <= "1A")){
+        out_file << "Error: Invalid number of registers for OPCode " << instr.opcode << " at line " << line_num << ".\n";
+        out_file << "Number of Registers Expected: 3, Found: " << instr.reg_num;
+        return;
+    }
+    else cout << "Number of registers valid for the given opcode\n";
 
     // Debugging Instruction
     cout << "Opcode: " << instr.opcode << ",\n";
@@ -158,7 +216,7 @@ void parse(int line_num, const string &line, ofstream &out_file) {
     cout << "Dataline: " << instr.dataline << "\n\n";
 }
 
-int main(){
+int main(int argc, char **argv){
     string input = "asmcode.txt";
     string output = "hexcode.txt";
 
@@ -192,6 +250,11 @@ int main(){
 
         else if (line.size() < 4){
             hexfile << "Error: Invalid line at line " << ++line_num << "\n";
+            continue;
+        }
+
+        else if (count(line.begin(), line.end(), ';') > 1){
+            hexfile << "Error: Too many semicolon on line " << ++line_num << ". Ensure only 1 semi-colon at the end of each line.\n";
             continue;
         }
         
