290ff5093586b967a7806c9684e1f6ad61be9c7a
Ashwamedh <113002273+Ashwamedh-14@users.noreply.github.com>
2025-02-27 20:15:36 +0530

    Testing

--- New Code/main.cpp
+++ New Code/main.cpp
@@ -14,7 +14,6 @@ The assembly code should be written in the following format:
     b. REGISTER1, REGISTER2, REGISTER3: The registers used in the instruction. The registers should be written in the format "R<register number>". The register number should be written in decimal.
     c. DATALINE: The data to be used in the instruction. It should be written in hexadecimal.
 3. The instruction should be followed by a semicolon.
-
 */
 
 #include <iostream>
