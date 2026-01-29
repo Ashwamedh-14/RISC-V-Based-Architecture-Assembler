91a4c6090e54d5e337cf926cbae5a83da07a034f
Ashwamedh <113002273+Ashwamedh-14@users.noreply.github.com>
2026-01-29 17:25:46 +0530

    Fixed a bug where the Assembler would throw error for a valid label starting with 'R'

--- src/assembler.cpp
+++ src/assembler.cpp
@@ -308,7 +308,7 @@ uint8_t parse(size_t line_num, const string &line, const string block_label, con
             return INVALID_LINE;
         }
 
-        else if (word[0] == 'R' && !isValidLabel(word.substr(1))) instr.dataline = word;
+        else if (word[0] == 'R' && !validReg(word)) instr.dataline = word;
         
         else if (word[0] == 'R' && instr.reg_num >= 3){
             if (!block_label.empty()) out_file << "In block: " << block_label << ".\n";
